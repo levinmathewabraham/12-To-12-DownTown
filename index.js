3b01731a33dc233c2ddde3beb3880403e7fab937
Levin Mathew Abraham <levinabraham2819@gmail.com>
2024-08-04 00:03:28 +0530

    latest commit
    -updated menu sidebar, responsiveness, menu behaviors

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-/*function to hide and show cards in menu */
 let isallshown = false;
-  var menusection = document.querySelectorAll(".items");
-  function showhide(item){
-    if(isallshown){
-      
-      item.classList.toggle('hide');
-      isallshown =!isallshown;
-    }
-    else{
-      showhideall();
-      item.classList.toggle('hide');
-      isallshown=!isallshown;
-    }
+var menusection = document.querySelectorAll(".items");
+var sidebar = document.getElementById('menuSidebar');
+
+function showhide(item){
+  menusection.forEach(section => section.classList.add('hide'));
+  item.classList.remove('hide');
+  isallshown = false;
+
+  if (window.innerWidth <= 768) {
+    sidebar.classList.remove('show');
   }
+}
  
-  function showhideall(){
-    isallshown = !isallshown;
-    for(var i=0;i<menusection.length;i++)
-  {
-    
-    menusection[i].classList.toggle('hide',isallshown);
+function showhideall() {
+  menusection.forEach(section => section.classList.remove('hide'));
+  isallshown = true;
+
+  if (window.innerWidth <= 768) {
+    sidebar.classList.remove('show');
   }
+}
+
+function toggleSidebar() {
+  if (window.innerWidth <= 768) {
+    sidebar.classList.toggle('show');
   }
+}
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth > 768) {
+    sidebar.classList.add('show');
+  }
+});
